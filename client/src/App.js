69ee1b67d3700418cdd5efa442924979c06f79f9
Boy Adisorn <boyadisorn@Boys-MacBook-Air.local>
2022-11-01 19:30:42 +0700

    feat:homepage/rooms_details done

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { CircularProgress, CircularProgressLabel } from "@chakra-ui/react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
-import { Text } from "@chakra-ui/react";
+import RoomsDetails from "./components/RoomsDetails.js";
 function App() {
   return (
     <div className="App">
-      <Text fontSize="5xl">Neatly Hotel</Text>
-      <Text>TEST TESTSTSTSTSTSTSTSTSTS</Text>
-      <Text
-        fontWeight="bold
-      "
-      >
-        Service & Facilities
-      </Text>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RoomsDetails />
     </div>
   );
 }
---
+++ client/src/components/RoomsDetails.js
@@ -0,0 +1,139 @@
+import { Text } from "@chakra-ui/react";
+import { Flex, Spacer } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+
+function RoomsDetails() {
+  return (
+    <Flex direction="column" align="center" margin="25px">
+      <Text fontSize="6xl" color="#2F3E35" textAlign="center" fontWeight="bold">
+        Rooms & Suits
+      </Text>
+      <Flex position="relative">
+        <Flex
+          zIndex="popover"
+          position="absolute"
+          bottom="100px"
+          left="80px"
+          color="white"
+          direction="column"
+        >
+          <Text fontSize="3rem">Superior Garden View</Text>
+          <Flex position="relative" left="10px" _hover={{ color: "orange" }}>
+            <Text fontSize="1.5rem">Explore room</Text>
+            <ArrowForwardIcon position="absolute" left="145px" bottom="8px" />
+          </Flex>
+        </Flex>
+        <Image
+          src="./images/rooms_details/superior_garden_view.svg"
+          alt="Superior Garden View"
+          margin="15px"
+        />
+      </Flex>
+      <Flex position="relative">
+        <Flex
+          zIndex="popover"
+          position="absolute"
+          bottom="100px"
+          left="80px"
+          color="white"
+          direction="column"
+        >
+          <Text fontSize="3rem">Deluxe</Text>
+          <Flex position="relative" left="10px" _hover={{ color: "orange" }}>
+            <Text fontSize="1.5rem">Explore room</Text>
+            <ArrowForwardIcon position="absolute" left="145px" bottom="8px" />
+          </Flex>
+        </Flex>
+        <Image
+          src="./images/rooms_details/deluxe.svg"
+          alt="Deluxe"
+          margin="15px"
+        />
+        <Flex
+          zIndex="popover"
+          position="absolute"
+          bottom="100px"
+          right="230px"
+          color="white"
+          direction="column"
+        >
+          <Text fontSize="3rem">Superior</Text>
+          <Flex position="relative" left="10px" _hover={{ color: "orange" }}>
+            <Text fontSize="1.5rem">Explore room</Text>
+            <ArrowForwardIcon position="absolute" left="145px" bottom="8px" />
+          </Flex>
+        </Flex>
+        <Image
+          src="./images/rooms_details/superior.svg"
+          alt="Superior"
+          margin="15px"
+        />
+      </Flex>
+      <Flex position="relative">
+        <Flex
+          zIndex="popover"
+          position="absolute"
+          bottom="100px"
+          left="80px"
+          color="white"
+          direction="column"
+        >
+          <Text fontSize="3rem">Premier Sea View</Text>
+          <Flex position="relative" left="10px" _hover={{ color: "orange" }}>
+            <Text fontSize="1.5rem">Explore room</Text>
+            <ArrowForwardIcon position="absolute" left="145px" bottom="8px" />
+          </Flex>
+        </Flex>
+
+        <Image
+          src="./images/rooms_details/premier_sea_view.svg"
+          alt="Premier Sea View"
+          margin="15px"
+        />
+        <Flex direction="column" position="relative">
+          <Flex
+            zIndex="popover"
+            position="absolute"
+            top="180px"
+            left="80px"
+            color="white"
+            direction="column"
+          >
+            <Text fontSize="3rem">Supreme</Text>
+            <Flex position="relative" left="10px" _hover={{ color: "orange" }}>
+              <Text fontSize="1.5rem">Explore room</Text>
+              <ArrowForwardIcon position="absolute" left="145px" bottom="8px" />
+            </Flex>
+          </Flex>
+          <Image
+            src="./images/rooms_details/supreme.svg"
+            alt="Supreme"
+            margin="15px"
+          />
+          <Flex
+            zIndex="popover"
+            position="absolute"
+            bottom="100px"
+            left="80px"
+            color="white"
+            direction="column"
+          >
+            <Text fontSize="3rem">Suite</Text>
+            <Flex position="relative" left="10px" _hover={{ color: "orange" }}>
+              <Text fontSize="1.5rem">Explore room</Text>
+              <ArrowForwardIcon position="absolute" left="145px" bottom="8px" />
+            </Flex>
+          </Flex>
+          <Image
+            src="./images/rooms_details/suite.svg"
+            alt="Suite"
+            margin="15px"
+          />
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+}
+
+export default RoomsDetails;
