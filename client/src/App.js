e1a11296a62c1aa532cc839de25b192fde428563
nan <manassa.p10@gmail.com>
2022-11-02 13:49:04 +0700

    feat:service and facilities

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import { CircularProgress, CircularProgressLabel } from "@chakra-ui/react";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
-import SearchSection from "./components/SearchSection.js";
+import Service from "./components/Service.js";
 
 function App() {
   return (
     <div className="App">
-      <SearchSection />
+      <Service/>
       <header></header>
     </div>
   );
