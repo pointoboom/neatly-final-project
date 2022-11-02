1e37e30b680f5d0208d61fc67fdf0f07e6e61d99
pointoboom <59865507+pointoboom@users.noreply.github.com>
2022-11-02 14:44:47 +0700

    feat:add search component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { CircularProgress, CircularProgressLabel } from "@chakra-ui/react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
-import { Text } from "@chakra-ui/react";
-import SearchSection from "./components/SearchSection";
+
 function App() {
-  return (
-    <div className="App">
-      <SearchSection />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
