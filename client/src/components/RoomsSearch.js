bb8c368847571dd602c15320210832979cc94ace
Boy Adisorn <boyadisorn@Boys-MacBook-Air.local>
2022-11-02 16:26:02 +0700

    finished

--- client/src/components/RoomsSearch.js
+++ client/src/components/RoomsSearch.js
@@ -21,31 +21,57 @@ function RoomsSearch() {
           height="250px"
         />
         <Flex direction="column" justify="center" ml="1rem" mb="2rem">
-          <Heading fontSize="xl">Superior Garden View</Heading>
-          <Text fontSize="xs" color="gray.700" fontWeight="thin" my="1rem">
+          <Heading fontSize="2xl" fontFamily={"Inter"}>
+            Superior Garden View
+          </Heading>
+          <Text
+            fontSize="s"
+            color="gray.700"
+            fontWeight="thin"
+            my="1rem"
+            fontFamily={"Inter"}
+          >
             2 Guests&ensp;|&ensp;1 Double bed&ensp;|&ensp;32 sqm
           </Text>
-          <Text fontSize="xs" fontFamily="inter">
+          <Text fontSize="s" fontFamily={"inter"}>
             Rooms (36sqm) with full garden views, 1<br /> single bed, bathroom
             with bathtub & <br />
             shower.
           </Text>
         </Flex>
-        <Flex direction="column" justify="center">
-          <Text fontSize="xs" as="s" textAlign="end">
+        <Flex direction="column" justify="center" mt="3rem">
+          <Text fontSize="s" as="s" textAlign="end" fontFamily={"Inter"}>
             THB 3,100.00
           </Text>
-          <Heading fontSize="sm" textAlign="end">
+          <Heading fontSize="sm" textAlign="end" fontFamily={"Inter"}>
             THB 2,500.00
           </Heading>
-          <Text fontSize="xs" mt="1rem" mb="3rem" textAlign="end">
+          <Text
+            fontSize="s"
+            mt="1rem"
+            mb="3rem"
+            textAlign="end"
+            fontFamily={"Inter"}
+          >
             Per Night <br /> (Including Taxes & Fees)
           </Text>
-          <Flex align="center" mb="2.25rem">
-            <Text color="orange.500" mr="2rem" fontWeight="semibold">
+          <Flex align="center" mb="1rem" pt="1rem">
+            <Text
+              color="orange.500"
+              mr="2rem"
+              fontWeight="semibold"
+              fontFamily={"Open Sans"}
+              fontSize="lg"
+            >
               Room Detail
             </Text>
-            <Button bg="#C14817" color="white" size="lg">
+            <Button
+              bg="#C14817"
+              color="white"
+              size="lg"
+              fontFamily={"Open Sans"}
+              fontSize="lg"
+            >
               Book Now
             </Button>
           </Flex>
@@ -60,33 +86,57 @@ function RoomsSearch() {
           height="250px"
         />
         <Flex direction="column" justify="center" ml="1rem" mb="2rem">
-          <Heading fontSize="xl" font-family="Inter">
+          <Heading fontSize="2xl" fontFamily={"Inter"}>
             Deluxe
           </Heading>
-          <Text fontSize="xs" color="gray.700" fontWeight="thin" my="1rem">
+          <Text
+            fontSize="s"
+            color="gray.700"
+            fontWeight="thin"
+            my="1rem"
+            fontFamily={"Inter"}
+          >
             2 Guests&ensp;|&ensp;1 Double bed&ensp;|&ensp;32 sqm
           </Text>
-          <Text fontSize="xs">
+          <Text fontSize="s" fontFamily={"Inter"}>
             Rooms (36sqm) with full garden views, 1<br /> single bed, bathroom
             with bathtub & <br />
             shower.
           </Text>
         </Flex>
-        <Flex direction="column" justify="center">
-          <Text fontSize="xs" as="s" textAlign="end">
+        <Flex direction="column" justify="center" mt="3rem">
+          <Text fontSize="s" as="s" textAlign="end" fontFamily={"Inter"}>
             THB 3,100.00
           </Text>
-          <Heading fontSize="sm" textAlign="end">
+          <Heading fontSize="sm" textAlign="end" fontFamily={"Inter"}>
             THB 2,500.00
           </Heading>
-          <Text fontSize="xs" mt="1rem" mb="3rem" textAlign="end">
+          <Text
+            fontSize="s"
+            mt="1rem"
+            mb="3rem"
+            textAlign="end"
+            fontFamily={"Inter"}
+          >
             Per Night <br /> (Including Taxes & Fees)
           </Text>
-          <Flex align="center" mb="2.25rem">
-            <Text color="orange.500" mr="2rem" fontWeight="semibold">
+          <Flex align="center" mb="1rem" pt="1rem">
+            <Text
+              color="orange.500"
+              mr="2rem"
+              fontWeight="semibold"
+              fontFamily={"Open Sans"}
+              fontSize="lg"
+            >
               Room Detail
             </Text>
-            <Button bg="#C14817" color="white" size="lg">
+            <Button
+              bg="#C14817"
+              color="white"
+              size="lg"
+              fontFamily={"Open Sans"}
+              fontSize="lg"
+            >
               Book Now
             </Button>
           </Flex>
@@ -101,33 +151,57 @@ function RoomsSearch() {
           height="150px"
         />
         <Flex direction="column" justify="center" ml="1rem" mb="2rem">
-          <Heading fontSize="xl" font-family="Inter">
+          <Heading fontSize="2xl" fontFamily={"Inter"}>
             Superior
           </Heading>
-          <Text fontSize="xs" color="gray.700" fontWeight="thin" my="1rem">
+          <Text
+            fontSize="s"
+            color="gray.700"
+            fontWeight="thin"
+            my="1rem"
+            fontFamily={"Inter"}
+          >
             2 Guests&ensp;|&ensp;1 Double bed&ensp;|&ensp;32 sqm
           </Text>
-          <Text fontSize="xs">
+          <Text fontSize="s" fontFamily={"Inter"}>
             Rooms (36sqm) with full garden views, 1<br /> single bed, bathroom
             with bathtub & <br />
             shower.
           </Text>
         </Flex>
-        <Flex direction="column" justify="center">
-          <Text fontSize="xs" as="s" textAlign="end">
+        <Flex direction="column" justify="center" mt="3rem">
+          <Text fontSize="s" as="s" textAlign="end" fontFamily={"Inter"}>
             THB 3,100.00
           </Text>
-          <Heading fontSize="sm" textAlign="end">
+          <Heading fontSize="sm" textAlign="end" fontFamily={"Inter"}>
             THB 2,500.00
           </Heading>
-          <Text fontSize="xs" mt="1rem" mb="3rem" textAlign="end">
+          <Text
+            fontSize="s"
+            mt="1rem"
+            mb="3rem"
+            textAlign="end"
+            fontFamily={"Inter"}
+          >
             Per Night <br /> (Including Taxes & Fees)
           </Text>
-          <Flex align="center" mb="2.25rem">
-            <Text color="orange.500" mr="2rem" fontWeight="semibold">
+          <Flex align="center" mb="1rem" pt="1rem">
+            <Text
+              color="orange.500"
+              mr="2rem"
+              fontWeight="semibold"
+              fontFamily={"Open Sans"}
+              fontSize="lg"
+            >
               Room Detail
             </Text>
-            <Button bg="#C14817" color="white" size="lg">
+            <Button
+              bg="#C14817"
+              color="white"
+              size="lg"
+              fontFamily={"Open Sans"}
+              fontSize="lg"
+            >
               Book Now
             </Button>
           </Flex>
@@ -142,33 +216,57 @@ function RoomsSearch() {
           height="150px"
         />
         <Flex direction="column" justify="center" ml="1rem" mb="2rem">
-          <Heading fontSize="xl" font-family="Inter">
+          <Heading fontSize="2xl" fontFamily={"Inter"}>
             Supreme
           </Heading>
-          <Text fontSize="xs" color="gray.700" fontWeight="thin" my="1rem">
+          <Text
+            fontSize="s"
+            color="gray.700"
+            fontWeight="thin"
+            my="1rem"
+            fontFamily={"Inter"}
+          >
             2 Guests&ensp;|&ensp;1 Double bed&ensp;|&ensp;32 sqm
           </Text>
-          <Text fontSize="xs">
+          <Text fontSize="s" fontFamily={"Inter"}>
             Rooms (36sqm) with full garden views, 1<br /> single bed, bathroom
             with bathtub & <br />
             shower.
           </Text>
         </Flex>
-        <Flex direction="column" justify="center">
-          <Text fontSize="xs" as="s" textAlign="end">
+        <Flex direction="column" justify="center" mt="3rem">
+          <Text fontSize="s" as="s" textAlign="end" fontFamily={"Inter"}>
             THB 3,100.00
           </Text>
-          <Heading fontSize="sm" textAlign="end">
+          <Heading fontSize="sm" textAlign="end" fontFamily={"Inter"}>
             THB 2,500.00
           </Heading>
-          <Text fontSize="xs" mt="1rem" mb="3rem" textAlign="end">
+          <Text
+            fontSize="s"
+            mt="1rem"
+            mb="3rem"
+            textAlign="end"
+            fontFamily={"Inter"}
+          >
             Per Night <br /> (Including Taxes & Fees)
           </Text>
-          <Flex align="center" mb="2.25rem">
-            <Text color="orange.500" mr="2rem" fontWeight="semibold">
+          <Flex align="center" mb="1rem" pt="1rem">
+            <Text
+              color="orange.500"
+              mr="2rem"
+              fontWeight="semibold"
+              fontFamily={"Open Sans"}
+              fontSize="lg"
+            >
               Room Detail
             </Text>
-            <Button bg="#C14817" color="white" size="lg">
+            <Button
+              bg="#C14817"
+              color="white"
+              size="lg"
+              fontFamily={"Open Sans"}
+              fontSize="lg"
+            >
               Book Now
             </Button>
           </Flex>
