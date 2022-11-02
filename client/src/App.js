4ca7546e061089fd893ef1a0629ed3e3a5b0cdc2
wat <twatcharin90@gamil.com>
2022-11-02 11:17:53 +0700

    feat: create HomePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,17 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import { CircularProgress, CircularProgressLabel } from "@chakra-ui/react";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
 import SliderImage from "./components/SliderImage";
-import About from "./components/About";
 
-import CustomerCarousel from "./components/CustomerCarousel";
-// import TestCompoent from "./components/TestCompoent";
+import HomePage from "./HomePage";
+
 
 function App() {
   return (
     <div className="App">
-      <About />
-      <SliderImage />
-
-      <CustomerCarousel/>
+      <HomePage />
 
     </div>
   );
