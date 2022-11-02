4a3e26e3583c51cf5011a4a03bd72939ece2eadc
nan <manassa.p10@gmail.com>
2022-11-02 10:33:27 +0700

    feat:search section without calendar

--- client/src/App.js
+++ client/src/App.js
@@ -3,37 +3,13 @@ import "./App.css";
 import { CircularProgress, CircularProgressLabel } from "@chakra-ui/react";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
+import SearchSection from "./components/SearchSection.js";
+
 function App() {
   return (
     <div className="App">
-      <Text
-        fontSize="5xl"
-        fontWeight="400px"
-        fontFamily={("Noto Serif Display", "serif")}
-      >
-        Neatly Hotel
-      </Text>
-      <Text>TEST TESTSTSTSTSTSTSTSTSTS</Text>
-      <Text
-        fontWeight="bold
-      "
-      >
-        Service & Facilities
-      </Text>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchSection />
+      <header></header>
     </div>
   );
 }
