8e672ad396487847867a1ab7c7ecf43281276b3e
KwanDT <111258541+KwanDT@users.noreply.github.com>
2022-11-03 16:06:24 +0700

    Update auth.js

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -19,7 +19,8 @@ const newUser = {
         ...req.body,
      };
 const salt = await bcrypt.genSalt(10);
-     newUser.password = await bcrypt.hash(newUser.password, salt);
+     
+newUser.password = await bcrypt.hash(newUser.password, salt);
      await pool.query(
        `insert into users
        (fullname, username, email, password, id_number, date_of_birth,
