827ef6c454248824f08adcd2b81618718e6af856
pointoboom <59865507+pointoboom@users.noreply.github.com>
2022-11-03 12:05:34 +0700

    feat:create register page

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,6 +1,25 @@
 import { Flex, Box, Text, Button, Input } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
+import { countryList } from "../data/country";
+import { DatePicker, Select, Upload } from "antd";
+import React, { useState } from "react";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+import "antd/dist/antd.css";
 function RegisterPage() {
+  const [imageUrl, setImageUrl] = useState();
+  const [loading, setLoading] = useState(false);
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
+      </div>
+    </div>
+  );
   return (
     <>
       <Navbar />
@@ -14,12 +33,14 @@ function RegisterPage() {
         backgroundRepeat="no-repeat"
         bgSize="cover"
         pt="60px"
+        pb="100px"
       >
         <Flex
           className="register-container"
           bg="white"
           p="80px"
           direction="column"
+          borderRadius="5px"
         >
           <Flex className="register-title">
             <Text
@@ -32,7 +53,154 @@ function RegisterPage() {
               Register
             </Text>
           </Flex>
-          <Flex className="info-form"></Flex>
+          <Flex className="info-form" direction="column" mb="40px">
+            <Flex mb="40px">
+              <Text
+                fontFamily={"Inter"}
+                fontSize="20px"
+                color="gray.600"
+                fontStyle="600"
+              >
+                Basic Information
+              </Text>
+            </Flex>
+            <Flex
+              direction="column"
+              fontFamily={"Inter"}
+              fontSize="16px"
+              mb="40px"
+            >
+              <Text mb="15px">Fullname</Text>
+              <Input
+                placeholder="Enter your username"
+                width="full"
+                fontFamily={"Inter"}
+                fontSize="16px"
+              ></Input>
+            </Flex>
+            <Flex>
+              <Flex direction="column" mr="50px">
+                <Flex
+                  direction="column"
+                  fontFamily={"Inter"}
+                  fontSize="16px"
+                  mb="40px"
+                >
+                  <Text mb="15px">Username</Text>
+                  <Input
+                    placeholder="Enter your username"
+                    width="550px"
+                    fontFamily={"Inter"}
+                    fontSize="16px"
+                  ></Input>
+                </Flex>
+                <Flex
+                  direction="column"
+                  fontFamily={"Inter"}
+                  fontSize="16px"
+                  mb="40px"
+                >
+                  <Text mb="15px">Password</Text>
+                  <Input
+                    placeholder="Enter your password"
+                    width="550px"
+                    fontFamily={"Inter"}
+                    fontSize="16px"
+                  ></Input>
+                </Flex>
+                <Flex direction="column" fontFamily={"Inter"} fontSize="16px">
+                  <Text mb="15px">Date od Birth</Text>
+                  <DatePicker
+                    format="dd,DD MMM YYYY"
+                    style={{
+                      width: "550px",
+                      fontFamily: "Inter",
+                      fontSize: "30px",
+                      paddingLeft: "15px",
+                    }}
+                    placeholder="Enter your username"
+                  />
+                </Flex>
+              </Flex>
+              <Flex direction="column">
+                <Flex
+                  direction="column"
+                  fontFamily={"Inter"}
+                  fontSize="16px"
+                  mb="40px"
+                >
+                  <Text mb="15px">Email</Text>
+                  <Input
+                    placeholder="Enter your email"
+                    width="550px"
+                    fontFamily={"Inter"}
+                    fontSize="16px"
+                  ></Input>
+                </Flex>
+                <Flex
+                  direction="column"
+                  fontFamily={"Inter"}
+                  fontSize="16px"
+                  mb="40px"
+                >
+                  <Text mb="15px">Id Number</Text>
+                  <Input
+                    placeholder="Enter your ID Number"
+                    width="550px"
+                    fontFamily={"Inter"}
+                    fontSize="16px"
+                  ></Input>
+                </Flex>
+                <Flex direction="column" fontFamily={"Inter"} fontSize="16px">
+                  <Text mb="15px">Country</Text>
+                  <Select
+                    placeholder="Select your country"
+                    options={countryList.map((item) => ({
+                      label: item.name,
+                      value: item.name,
+                    }))}
+                    style={{
+                      fontFamily: "Inter",
+                      fontSize: "16px",
+                    }}
+                  ></Select>
+                </Flex>
+              </Flex>
+            </Flex>
+          </Flex>
+          <Flex className="picture-upload" direction="column" mb="40px">
+            <Flex mb="40px">
+              <Text
+                fontFamily={"Inter"}
+                fontSize="20px"
+                color="gray.600"
+                fontStyle="600"
+              >
+                Profile Picture
+              </Text>
+            </Flex>
+            <Upload
+              name="avatar"
+              listType="picture-card"
+              className="avatar-uploader"
+              showUploadList={false}
+              // action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+              // beforeUpload={beforeUpload}
+              // onChange={handleChange}
+            >
+              {imageUrl ? (
+                <img
+                  src={imageUrl}
+                  alt="avatar"
+                  style={{
+                    width: "100%",
+                  }}
+                />
+              ) : (
+                uploadButton
+              )}
+            </Upload>
+          </Flex>
           <Flex className="card-form" direction="column" mb="60px">
             <Flex mb="40px">
               <Text
