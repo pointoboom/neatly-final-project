fdb07d0b648d24d2d82ffacbe424c3094879b52d
nan <manassa.p10@gmail.com>
2022-11-08 13:54:55 +0700

    fix: rebase

--- client/src/pages/UnauthenticatedApp.js
+++ client/src/pages/UnauthenticatedApp.js
@@ -4,6 +4,8 @@ import HomePage from "./HomePage";
 import RegisterPage from "./RegisterPage";
 import RoomSearchPage from "./RoomSearchPage";
 import LoginPage from "./LoginPage";
+import ReservationPage from "./ReservationPage";
+
 
 function UnauthenticatedApp() {
   return (
@@ -15,6 +17,8 @@ function UnauthenticatedApp() {
         {/* <Route path="/createorder" element={<CreateOrders />} /> */}
         {/* <Route path="*" element={<HomePage />} /> */}
         <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="*" element={<LoginPage />} /> */}
+        <Route path="/reservation" element={<ReservationPage />} />
       </Routes>
     </div>
   );
