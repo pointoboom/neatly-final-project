5f627c511cc9d9483d00bcd9b79b9d69744d6b2c
KwanDT <111258541+KwanDT@users.noreply.github.com>
2022-11-11 15:55:05 +0700

    Create Sidebar.js
    feat: Admin Sidebar

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,61 @@
+import { Box, Flex, VStack, Image, Text} from "@chakra-ui/react";
+function SideBar() {
+    return (
+    <Flex backgroundColor="#2F3E35" w="240px" h="1024px" pt={"48px"} 
+    display="flex" direction="column" justify="flex-start">
+    <VStack>
+        <Image src="./images/whitelogo.svg" alt="Neatly Logo" w="150px" h="30px"/>
+        <Text pt={"2px"} pb={"80px"} font-family="Inter" font-style="normal" font-weight="400"
+        font-size="16px" line-height="150%" letter-spacing="-0.02em" color="#ABC0B4">
+        Admin Panel Control</Text>
+    <Box w="240px" h="72px"  p="30px" 
+    _hover={{background: "#465C50"}} _selected={{background: "#5D7B6A"}}>
+    <Flex display="flex" direction="row">
+        <Image
+              boxSize="27px" pr="10px"
+              src="./images/booking.svg"/>
+        <Text font-family="Inter" font-style="normal" font-weight="500" font-size="16px"
+        line-height="150%" letter-spacing="-0.02em" textAlign={"center"} color="#F1F5F3">
+        Customer Booking</Text></Flex></Box>
+    <Box w="240px" h="72px"  p="30px" gap="0"
+    _hover={{background: "#465C50"}} _selected={{background: "#5D7B6A"}}>
+    <Flex display="flex" direction="row">
+        <Image
+              boxSize="27px" pr="10px"
+              src="./images/manage.svg"/>
+        <Text font-family="Inter" font-style="normal" font-weight="500" font-size="16px"
+        line-height="150%" letter-spacing="-0.02em" textAlign={"center"} color="#F1F5F3">
+        Room Management</Text></Flex></Box>
+    <Box w="240px" h="72px"  p="30px" gap="0"
+    _hover={{background: "#465C50"}} _selected={{background: "#5D7B6A"}}>
+    <Flex display="flex" direction="row">
+        <Image
+              boxSize="27px" pr="10px"
+              src="./images/hotel.svg"/>
+        <Text font-family="Inter" font-style="normal" font-weight="500" font-size="16px"
+        line-height="150%" letter-spacing="-0.02em" textAlign={"center"} color="#F1F5F3">
+        Hotel Information</Text></Flex></Box>
+    <Box w="240px" h="72px"  p="30px" gap="0" 
+    _hover={{background: "#465C50"}} _selected={{background: "#5D7B6A"}}>
+    <Flex display="flex" direction="row">
+        <Image
+              boxSize="27px" pr="10px"
+              src="./images/room.svg"/>
+        <Text font-family="Inter" font-style="normal" font-weight="500" font-size="16px"
+        line-height="150%" letter-spacing="-0.02em" textAlign={"center"} color="#F1F5F3">
+        Room & Property</Text></Flex></Box><Box w="240px" h="292px"></Box>
+    <Box w="240px" h="72px"  p="30px" gap="0" borderTop="1px" borderColor="#465C50"
+    _hover={{background: "#465C50"}} _selected={{background: "#5D7B6A"}}>
+    <Flex display="flex" direction="row">
+        <Image
+              boxSize="27px" pr="10px"
+              src="./images/logout.svg"/>
+        <Text font-family="Inter" font-style="normal" font-weight="500" font-size="16px"
+        line-height="150%" letter-spacing="-0.02em" textAlign={"center"} color="#F1F5F3">
+        Log Out</Text></Flex></Box>
+    </VStack>
+    </Flex>
+  );
+}
+
+export default SideBar;
