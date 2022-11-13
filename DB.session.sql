00f12025d9a4668892fb2cf971622351ea08a95b
wat <twatcharin90@gamil.com>
2022-11-13 14:14:59 +0700

    feat: show room random other rooms
     nteractive rebase in progress; onto c671445

--- DB.session.sql
+++ DB.session.sql
@@ -1,2 +1,9 @@
+<<<<<<< HEAD
 
 select * FROM users
+=======
+SELECT * FROM public.room_types
+WHERE room_types_id <> 1
+ORDER BY RANDOM()
+LIMIT 2
+>>>>>>> 7126cf4 (feat: show room random other rooms)
