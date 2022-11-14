81597d080ed5c67cf303c20225e2003ca4a1a84f
wat <twatcharin90@gamil.com>
2022-11-14 09:57:38 +0700

    feat: inser reservations to db

--- DB.session.sql
+++ DB.session.sql
@@ -1,1 +1,10 @@
-select * FROM room_types
+SELECT * FROM reservations
+LEFT  JOIN reservations_request 
+ON reservations.reservation_id = reservations_request.reservation_id
+LEFT  JOIN request
+ON reservations_request.request_id = request.request_id
+LEFT  JOIN bills
+ON reservations.reservation_id = bills.reservation_id
+where reservations.reservation_id = 27
+
+
--- server/apps/reservation.js
+++ server/apps/reservation.js
@@ -25,10 +25,91 @@ reserveRouter.post("/", async (req, res) => {
       confirm: true,
     });
     console.log("Payment", payment);
-    res.json({
-      message: "Payment successful",
-      success: true,
-    });
+
+    ////// insert db
+
+    if (payment.status == "succeeded") {
+      const newReservations = {
+        ...req.body,
+        booking_date: new Date(),
+        payment_method: "credit card",
+      };
+
+      console.log("newReservations", newReservations);
+
+      const reservationsid = await pool.query(
+        `insert into reservations
+             (booking_date, check_in_date, check_out_date, room_type_id, amount, guest_no,payment_method)
+             VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
+        [
+          newReservations.booking_date,
+          newReservations.checkIn,
+          newReservations.checkOut,
+          newReservations.roomId,
+          newReservations.amountRoom,
+          newReservations.guest,
+          newReservations.payment_method,
+        ]
+      );
+
+      console.log(reservationsid.rows[0].reservation_id);
+      // console.log(reservationsid.rows[0].userId);
+
+      newReservations.specialRequest.map(async (item) => {
+        const request_id = await pool.query(
+          `SELECT * FROM request where have = $1 `,
+          [item.req]
+        );
+        // console.log(request_id.rows[0].request_id);
+
+        await pool.query(
+          `insert into reservations_request
+         (reservation_id, request_id)
+         VALUES ($1, $2) `,
+          [reservationsid.rows[0].reservation_id, request_id.rows[0].request_id]
+        );
+      });
+
+      newReservations.standardRequest.map(async (item) => {
+        const request_id = await pool.query(
+          `SELECT * FROM request where have = $1 `,
+          [item.standard_req]
+        );
+        // console.log(request_id.rows[0].request_id);
+
+        await pool.query(
+          `insert into reservations_request
+         (reservation_id, request_id)
+         VALUES ($1, $2) `,
+          [reservationsid.rows[0].reservation_id, request_id.rows[0].request_id]
+        );
+      });
+
+      await pool.query(
+        `insert into bills
+         (reservation_id, total_price)
+         VALUES ($1, $2) `,
+        [reservationsid.rows[0].reservation_id, newReservations.sumPrice]
+      );
+
+      await pool.query(
+        `insert into users_reservations
+         (reservation_id, user_id)
+         VALUES ($1, $2) `,
+        [reservationsid.rows[0].reservation_id, newReservations.userId]
+      );
+
+      res.json({
+        message: "Payment successful",
+        success: true,
+        reservationsid: reservationsid.rows[0].reservation_id,
+      });
+    } else {
+      res.json({
+        message: "Payment failed",
+        success: false,
+      });
+    }
   } catch (error) {
     // console.log("Error", error);
     res.json({
