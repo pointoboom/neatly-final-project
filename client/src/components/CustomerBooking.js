c4c773942903634f0cca00015cce9288263005b4
Boy Adisorn <boyadisorn@Boys-MacBook-Air.local>
2022-11-15 16:11:17 +0700

    fix: rebase conflict

---
+++ client/src/components/CustomerBooking.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Flex, Text, Input } from "@chakra-ui/react";
+
+function CustomerBooking() {
+  return (
+    <Flex>
+      <Text>Customer Booking</Text>
+      <Input placeholder="medium size" size="md" />
+    </Flex>
+  );
+}
+
+export default CustomerBooking;
