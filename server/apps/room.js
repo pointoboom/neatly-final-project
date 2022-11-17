b620bc676ecc77d045564e22fc66f011d991f2d4
pointoboom <59865507+pointoboom@users.noreply.github.com>
2022-11-17 09:52:27 +0700

    feat:create manage api

--- server/apps/room.js
+++ server/apps/room.js
@@ -4,7 +4,9 @@ const roomRouter = Router();
 
 roomRouter.get("/", async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM public.room_types ORDER BY room_types_id ASC ");
+    const result = await pool.query(
+      "SELECT * FROM public.room_types ORDER BY room_types_id ASC "
+    );
     return res.json({
       data: result.rows,
     });
@@ -18,6 +20,21 @@ roomRouter.get("/:roomid", async (req, res) => {
     const result = await pool.query(
       "select type_name,promotion_price from room_types where room_types_id = $1",
       [roomid]
+    );
+    return res.json({
+      data: result.rows,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+roomRouter.get("/manage", async (req, res) => {
+  try {
+    const roomid = req.params.roomid;
+
+    const result = await pool.query(
+      "SELECT room_no,type_name,bed_type,status_name FROM room_managements left join room_types on room_managements.room_types_id = room_types.room_types_id left join status on room_managements.status_id = status.status_id"
     );
     return res.json({
       data: result.rows,
