40c82d951cb581918898abde1d742eb3b22ce46f
pointoboom <59865507+pointoboom@users.noreply.github.com>
2022-11-17 21:59:48 +0700

    Revert "Refactor reg"

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./contexts/authentication";
-import jwtInterceptor from "./utils/jwtInterceptor.js";
-import { HotelProvider } from "./contexts/reservation";
-jwtInterceptor();
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <HotelProvider>
-        <AuthProvider>
-          <ChakraProvider>
-            <App />
-          </ChakraProvider>
-        </AuthProvider>
-      </HotelProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
