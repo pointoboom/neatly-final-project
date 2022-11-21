5afea8f2b52094b8bdc8a357a4add77e08a4a066
Boy Adisorn <boyadisorn@Boys-MacBook-Air.local>
2022-11-21 13:55:50 +0700

    fix:style customer booking

--- client/src/pages/admins/CustomerBooking.js
+++ client/src/pages/admins/CustomerBooking.js
@@ -42,49 +42,71 @@ function CustomerBooking() {
   }, []);
 
   return (
-    <Flex direction="row">
+    <Flex direction="row" h="100vh" bg="#F6F7FC">
       <Sidebar />
-      <Flex direction="column" my="10">
-        <Flex mb="10">
-          <Text ml="20" fontWeight="semibold" mr="550">
-            Customer Booking
-          </Text>
-          <InputGroup>
-            <InputLeftElement children={<SearchIcon color="gray.400" />} />
-            <Input mr="10" placeholder="Search..." size="md" width="400px" />
-          </InputGroup>
-        </Flex>
-        <Flex align="center" justify="center" display="flex">
-          <TableContainer>
-            <Table variant="simple">
-              <Thead bg="#E4E6ED">
-                <Tr>
-                  <Th>Customer Name</Th>
-                  <Th>Guest(s)</Th>
-                  <Th>Roomtype</Th>
-                  <Th>Amount</Th>
-                  <Th>Bed-Type</Th>
-                  <Th>Check-in</Th>
-                  <Th>Check-out</Th>
-                </Tr>
-              </Thead>
-              {customerBooking.map((data) => {
-                return (
-                  <Tbody bg="white">
-                    <Tr>
-                      <Td>{data.fullname}</Td>
-                      <Td>{data.guest}</Td>
-                      <Td>{data.type_name}</Td>
-                      <Td>{data.total_price}</Td>
-                      <Td>{data.bed_type}</Td>
-                      <Td>{data.check_in_date}</Td>
-                      <Td>{data.check_out_date}</Td>
-                    </Tr>
-                  </Tbody>
-                );
-              })}
-            </Table>
-          </TableContainer>
+      <Flex w="100%" h="100vh" bg="blue" justifyContent="center">
+        <Flex direction="column" bg="#F6F7FC" w="full">
+          <Flex
+            h="80px"
+            bg="white"
+            mb="5px"
+            display="flex"
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Flex>
+              <Text fontSize="20px" ml="60px" fontWeight="semibold">
+                Customer Booking
+              </Text>
+            </Flex>
+            <Flex>
+              <InputGroup>
+                <InputLeftElement children={<SearchIcon color="gray.400" />} />
+                <Input
+                  mr="10"
+                  placeholder="Search..."
+                  size="md"
+                  width="400px"
+                />
+              </InputGroup>
+            </Flex>
+          </Flex>
+
+          <Flex align="center" justify="center" display="flex">
+            <TableContainer w="1080px" mt="50px">
+              <Table variant="simple" size="md" align="center">
+                <Thead bg="#CCCCCC" color="gray.800" mt="50px">
+                  <Tr>
+                    <Th>Customer Name</Th>
+                    <Th>Guest(s)</Th>
+                    <Th>Roomtype</Th>
+                    <Th>Amount</Th>
+                    <Th>Bed-Type</Th>
+                    <Th>Check-in</Th>
+                    <Th>Check-out</Th>
+                  </Tr>
+                </Thead>
+                {customerBooking.map((data) => {
+                  return (
+                    <Tbody bg="white">
+                      <Tr>
+                        <Td>{data.fullname}</Td>
+                        <Td>{data.guest}</Td>
+                        <Td>{data.type_name}</Td>
+                        <Td>{data.total_price}</Td>
+                        <Td>{data.bed_type}</Td>
+                        <Td>{data.check_in_date}</Td>
+                        <Td>{data.check_out_date}</Td>
+                      </Tr>
+                    </Tbody>
+                  );
+                })}
+
+                <Tbody bg="white"></Tbody>
+              </Table>
+            </TableContainer>
+          </Flex>
         </Flex>
       </Flex>
     </Flex>
