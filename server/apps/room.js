7ccdb36bdd07bebfd93c1b76660bcd281c97020f
pointoboom <59865507+pointoboom@users.noreply.github.com>
2022-11-29 09:46:30 +0700

    feat:fix amenity

--- server/apps/room.js
+++ server/apps/room.js
@@ -8,6 +8,7 @@ import {
   EditRoomManagement,
   GetRoomByID,
   SearchRoom,
+  DeleteRoomById,
 } from "../controllers/room.controller.js";
 import multer from "multer";
 const multerUpload = multer({ dest: "uploads/" });
@@ -43,6 +44,32 @@ roomRouter.post("/newroom", avatarUpload, async (req, res) => {
 
 roomRouter.put("/:roomid", avatarUpload, async (req, res) => {
   console.log(req.params.roomid);
+  const result = await pool.query(
+    `update room_types set type_name=$1, room_size=$2, bed_type=$3, guest=$4, max_guest=$5, no_of_bed=$6,
+             price=$7, promotion_price=$8, description=$9, amenity=$10, admin_id=$11, main_images=$12, gallery_images=$13,main_image_url=$14,gallery_images_id=$15,amenity_id=$16  where room_types_id = $17`,
+    [
+      newRoom.room_type,
+      `${newRoom.room_size} sqm`,
+      newRoom.bed_type,
+      newRoom.guest,
+      newRoom.guest,
+      newRoom.guest,
+      newRoom.price,
+      newRoom.price - 500,
+      newRoom.description,
+      newRoom.amenity,
+      newRoom.user_id,
+      mainImg,
+      galImg,
+      "test",
+      "test",
+      1,
+      roomId,
+    ]
+  );
+});
+roomRouter.delete("/:roomid", avatarUpload, async (req, res) => {
+  DeleteRoomById(req, res);
 });
 
 export default roomRouter;
